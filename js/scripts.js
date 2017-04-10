036eb77547c2b24f51c325d07844e208ecf5522a
Aubrey Masten and Kat Gifford <aubrey@>
2017-04-10 11:58:19 -0700

    add create grid

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 //back-end
-var System(){
+function System(){
   this.grid = [];
   this.coords = [];
+  this.gridSize = 3;
   //need to initialize data mambers for
   //audio context and oscillator node
 
@@ -9,8 +10,16 @@ var System(){
 
 }
 
-System.prototype.playSound(){}
-System.prototype.generateArray(){}
+System.prototype.playSound = function(){}
+System.prototype.generateArray = function(){
+  for (var i = 0; i < this.gridSize; i++){
+    this.grid.push([]);
+    for (var n = 0; n < this.gridSize; n++){
+      this.grid[i].push([]);
+    }
+  }
+  console.log(this.grid);
+}
 
 //front-end
 //this function is for the future. this is where we will generate any DOM elements
@@ -18,5 +27,7 @@ var generateDomGrid = function(){}
 
 $(document).ready(function(){
   //new is apparently a bad way of initializing objects
-  var system = new System();
+  var newSystem = new System();
+
+  newSystem.generateArray();
 });
