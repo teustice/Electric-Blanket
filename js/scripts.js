d3d403fe2c87bdd0286a70acb35ac381ff2cdea3
Aubrey Masten <mastenaub@gmail.com>
2017-04-10 11:05:54 -0700

    add system constructor and methods

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,22 @@
-EVERYTHING HAS CHANGEDEVERYTHING HAS CHANGEDEVERYTHING HAS CHANGED
-EVERYTHING HAS CHANGEDEVERYTHING HAS CHANGEDEVERYTHING HAS CHANGED
-EVERYTHING HAS CHANGEDEVERYTHING HAS CHANGEDEVERYTHING HAS CHANGED
-EVERYTHING HAS CHANGEDEVERYTHING HAS CHANGEDEVERYTHING HAS CHANGED
-EVERYTHING HAS CHANGEDEVERYTHING HAS CHANGEDEVERYTHING HAS CHANGED
-EVERYTHING HAS CHANGEDEVERYTHING HAS CHANGEDEVERYTHING HAS CHANGED
-EVERYTHING HAS CHANGEDEVERYTHING HAS CHANGEDEVERYTHING HAS CHANGED
-EVERYTHING HAS CHANGEDEVERYTHING HAS CHANGEDEVERYTHING HAS CHANGED
-EVERYTHING HAS CHANGEDEVERYTHING HAS CHANGEDEVERYTHING HAS CHANGED
+//back-end
+var System(){
+  this.grid = [];
+  this.coords = [];
+  //need to initialize data mambers for
+  //audio context and oscillator node
+
+
+
+}
+
+System.prototype.playSound(){}
+System.prototype.generateArray(){}
+
+//front-end
+//this function is for the future. this is where we will generate any DOM elements
+var generateDomGrid = function(){}
+
+$(document).ready(function(){
+  //new is apparently a bad way of initializing objects
+  var system = new System();
+});
