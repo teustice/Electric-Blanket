78a9443a454de647012ff44b0c844f00edc4a707
Aubrey Masten and Tanner Eustice <aubrey@>
2017-04-11 10:17:30 -0700

    add json data object

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,6 @@
 //back-end
-function System(){
-  this.grid = [];
-  this.coords = [0,0];
-  this.gridSize = 3;
-  this.conductor = null;
-  this.player = null;
-}
-
-System.prototype.initializeConductor = function(){
-  this.conductor = new BandJS();
-}
-System.prototype.reinitializeConductor = function(){
-  this.conductor.audioContext.close();
-  this.conductor.destroy();
-}
-
-System.prototype.staticNotes = function(){
-  this.grid[0][0] = "C4";
-  this.grid[0][1] = "G4";
-  this.grid[0][2] = "D4";
-  this.grid[1][0] = "A4";
-  this.grid[1][1] = "E3";
-  this.grid[1][2] = "E6";
-  this.grid[2][0] = "B3";
-  this.grid[2][1] = "B2";
-  this.grid[2][2] = "A6";
-}
-
-System.prototype.staticJSON = function(){
-  var json = {
+function Data() {
+  this.json = {
     timeSignature: [4, 4],
     tempo: 100,
     instruments: {
@@ -59,44 +31,47 @@ System.prototype.staticJSON = function(){
       ]
     }
   };
-  this.grid[0][0] = json;
-  this.grid[0][1] = json;
-  this.grid[0][2] = json;
-  this.grid[1][0] = json;
-  this.grid[1][1] = json;
-  this.grid[1][2] = json;
-  this.grid[2][0] = json;
-  this.grid[2][1] = json;
-  this.grid[2][2] = json;
 }
 
-//we now use json to store the note and more
-// System.prototype.updateNote = function(){
-//   this.note = this.grid[this.coords[0]][this.coords[1]];
-// }
-//timeSig is a 2 elem array containing top and bottom values
-System.prototype.initializeSounds = function() {
-  this.player = this.conductor.load(this.grid[this.coords[0]][this.coords[1]]);
+function System(){
+  this.grid = [];
+  this.coords = [0,0];
+  this.gridSize = 3;
+  this.conductor = null;
+  this.player = null;
+}
 
+System.prototype.initializeConductor = function(){
+  this.conductor = new BandJS();
+}
+System.prototype.reinitializeConductor = function(){
+  this.conductor.audioContext.close();
+  this.conductor.destroy();
+}
+
+System.prototype.staticJSON = function(){
+  this.grid[0][0] = new Data();
+  this.grid[0][1] = new Data();
+  this.grid[0][2] = new Data();
+  this.grid[1][0] = new Data();
+  this.grid[1][1] = new Data();
+  this.grid[1][2] = new Data();
+  this.grid[2][0] = new Data();
+  this.grid[2][1] = new Data();
+  this.grid[2][2] = new Data();
+}
+
+System.prototype.initializeSounds = function() {
+  this.player = this.conductor.load(this.grid[this.coords[0]][this.coords[1]].json);
 }
 
 System.prototype.startSound = function(){
   this.player.play();
-  // this.player.loop(true);
+  this.player.loop(true);
 }
 
 System.prototype.stopSound = function(){
   this.player.stop(true);
-}
-
-System.prototype.generateArray = function(){
-  for (var i = 0; i < this.gridSize; i++){
-    this.grid.push([]);
-    for (var n = 0; n < this.gridSize; n++){
-      this.grid[i].push([]);
-    }
-  }
-  console.log(this.grid);
 }
 
 System.prototype.updateCoords = function(x,y){
@@ -118,27 +93,6 @@ System.prototype.generateArray = function(){
       this.grid[i].push(`-`);
     }
   }
-}
-
-System.prototype.updateCoords = function(x,y){
-  if(x === -1 && this.coords[0] > 0){
-    this.coords[0] += x;
-  } else if(y === -1 && this.coords[1] > 0) {
-    this.coords[1] += y;
-  } else if(x === 1 && this.coords[0] < (this.gridSize-1)) {
-    this.coords[0] += x;
-  } else if(y === 1 && this.coords[1] < (this.gridSize-1)) {
-    this.coords[1] += y;
-  }
-}
-
-System.prototype.updateGrid = function(){
-  for (var i = 0; i < this.gridSize; i++){
-    for (var n = 0; n < this.gridSize; n++){
-      this.grid[i][n] = "-";
-    }
-  }
-  this.grid[this.coords[0]][this.coords[1]] = "X"
 }
 
 //front-end
@@ -166,17 +120,6 @@ $(document).ready(function(){
   //new is apparently a bad way of initializing objects
   var newSystem = new System();
 
-
-
-  //Begin sequence
-  $("#play").click(function(){
-    newSystem.startSound();
-  });
-  //Stop sequence
-  $("#stop").click(function(){
-    newSystem.stopSound();
-  });
-
   newSystem.generateArray();
   newSystem.staticJSON();
   newSystem.initializeConductor();
@@ -201,7 +144,5 @@ $(document).ready(function(){
     newSystem.reinitializeConductor();
     newSystem.initializeSounds();
     newSystem.startSound();
-
   });
-
 });
